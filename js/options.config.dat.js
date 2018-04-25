7b1b02289c6afc337f43cf12e8283709ddd9f3a5
Cdeperth <chdeperthuis@yahoo.fr>
2018-04-25 12:07:53 +0200

    config options in earth-moon.html like earth-moon2.html

--- js/options.config.dat.js
+++ js/options.config.dat.js
@@ -1,155 +1,131 @@
-var options = {
-	NAME:"EARTH_MOON",  // name of Simulation 
-	NAME_SVG: "EM1",
-	version : 1,
-    framerate: 60,
-    G: 10, // 6.673 ^ -11 ?? 
-	
-    TRAILS_DISPLAY: true,
-    SHOW_DIED: false,
-    SHOW_LABELS: true, 
-    TRAILS_LENGTH: 1000, // Max length of Trails 
-	
-	SPECIFIC_MASS : 500, 
-    START_SPEED: 10,
-	RADIUS:10, 
-	
-    MOON_SPEED: 12, // initial Moon speed 
-	
-    MoveSpeed: 50,  // for moving by keyBoard Up / Down keys
-
-    MAX_DISTANCE: 300000,  // Max distance from center to update Movers 
-	
-	MASS_FACTOR     : 10, // ??
-	DISTANCE_FACTOR : 10,
-/*
-	MASS_FACTOR   : 100000, // 1:10 Gigatonnes = 1:10*10^9 tons = 1:10*10^12 kg ?? 
-	DISTANCE_FACTOR : 1000, // 1:1000 km = 1:10^6 meters
-	RADIUS_FACTOR   : 1000, // for display of size ? not in use for the moment
-*/
-};
 
 // LOAD / SAVE CONFIG
 
-var optionsSvgName = "options_EM1";
-// Force Reset Config ? 
-var httpReset = GET('R');
-if(httpReset){
-	console.log("Force Reset Initial Config:" + httpReset);
-}
-// LOAD CONFIG 
-else if (localStorage && localStorage.getItem(optionsSvgName)){
-    optionsSVG = JSON.parse(localStorage.getItem(optionsSvgName));
-	if(optionsSVG.NAME==options.NAME){
-		console.log("LOAD CONFIG:" + optionsSvgName);
-		for (var key in optionsSVG) {
-			console.log(key, optionsSVG[key]);
-			options.key = optionsSVG.key;
-			options[key] = optionsSVG[key];
+function InitGUIFromOptions(options){
+
+	var optionsSvgName = options.NAME_SVG ? options.NAME_SVG : "options_EM" ;
+
+	// Force Reset Config ? 
+	var httpReset = GET('R');
+	if(httpReset){
+		console.log("Force Reset Initial Config:" + httpReset);
+		console.log("Initial Config :", options);
+	}
+	// LOAD CONFIG 
+	else if (localStorage && localStorage.getItem(optionsSvgName)){
+		optionsSVG = JSON.parse(localStorage.getItem(optionsSvgName));
+		if(optionsSVG.NAME==options.NAME){
+			console.log("LOAD CONFIG:" + optionsSvgName);
+			for (var key in optionsSVG) {
+				console.log(key, optionsSVG[key]);
+				options.key = optionsSVG.key;
+				options[key] = optionsSVG[key];
+			}
+		} else {
+			console.log("BAD LOAD CONFIG:" + optionsSVG.NAME + " - EXPECTED:" + options.NAME + " > NO LOAD");
+			console.log(options);
 		}
 	} else {
-		console.log("BAD LOAD CONFIG:" + optionsSVG.NAME + " - EXPECTED:" + options.NAME + " > NO LOAD");
-		console.log(options);
+		console.log("No Saved Config:" + optionsSvgName);
+		console.log("Initial Config :", options);
 	}
-} else {
-	console.log("No Saved Config:" + optionsSvgName);
-}
-options.SAVECONFIG = function () {
-    // SVG CONFIG OPTIONS
-    localStorage.setItem(optionsSvgName, JSON.stringify(options));
-	console.log("Saved Config:" + optionsSvgName);
-}
 
-options.RESET = function () {
-    reset();
-}
+	options.SAVECONFIG = function () {
+		// SVG CONFIG OPTIONS
+		localStorage.setItem(optionsSvgName, JSON.stringify(options));
+		console.log("Saved Config:" + optionsSvgName);
+	}
+
+	// ---
+
+	options.RESET = function () {
+		reset();
+	}
 
 
-options.REMOVE_DYNAMICS = function () {
-    removeDynamicMovers();
-}
+	options.REMOVE_DYNAMICS = function () {
+		removeDynamicMovers();
+	}
 
-// dat GUI
-var gui = new dat.GUI();
-gui.remember(options);
+	// dat GUI
+	var gui = new dat.GUI();
+	gui.remember(options);
 
-var f = gui.addFolder('Environment');
-f.open();
-//f.add(options, 'framerate', 1, 120);
-f.add(options, 'G', 1, 100).name('Gravity');
+	var f = gui.addFolder('Environment');
+	f.open();
+	//f.add(options, 'framerate', 1, 120);
+	f.add(options, 'G', 1, 100).name('Gravity');
 
-/*
-	var fMoverCountE = f.add(options, 'MOVER_COUNT', 1, 128);
-	fMoverCountE.onFinishChange(function (value) {
-		// Fires when a controller loses focus.
+	/*
+		var fMoverCountE = f.add(options, 'MOVER_COUNT', 1, 128);
+		fMoverCountE.onFinishChange(function (value) {
+			// Fires when a controller loses focus.
+			//reset();
+		});
+	*/
+
+	f = gui.addFolder('Trails & Labels');
+	f.open();
+	f.add(options, 'TRAILS_DISPLAY');
+	f.add(options, 'TRAILS_LENGTH', 0, 10000);
+
+	f.add(options, 'SHOW_DIED');
+	f.add(options, 'SHOW_LABELS');
+
+	f = gui.addFolder('Mass and Radius for Dynamics (Right Clic)');
+	f.open();
+
+	f.add(options, 'SPECIFIC_MASS', .00001, 10000.0).name("MASS");
+	f.add(options, 'RADIUS', .001, 10.0);
+	f.add(options, 'START_SPEED', 1e-100, 20.0);
+	f.add(options, 'REMOVE_DYNAMICS').name('Remove dynamic balls');
+
+
+	f = gui.addFolder('Start');
+	f.open();
+
+	var fSpeedE = f.add(options, 'MOON_SPEED', 1e-100, 20.0);
+	fSpeedE.onFinishChange(function (value) {
 		//reset();
 	});
-*/
 
-f = gui.addFolder('Trails & Labels');
-f.open();
-f.add(options, 'TRAILS_DISPLAY');
-f.add(options, 'TRAILS_LENGTH', 0, 10000);
+	//var moveSpeed = 5;
+	gui.fSpeedMS = f.add(options, 'MoveSpeed', 1, 100);
+	gui.fSpeedMS.onFinishChange(function (value) {
+		//console.log(value);
+		gui.moveSpeed = Math.floor(options.MoveSpeed);
+		console.log("MoveSpeed: " + value);
+	});
 
-f.add(options, 'SHOW_DIED');
-f.add(options, 'SHOW_LABELS');
+	//f.add(options, 'AddBigStar').name('Add Big Star');
+	f.add(options, 'SAVECONFIG').name('SaveTheConf');
+	f.add(options, 'RESET').name('RESET ALL');
+	f.add(options, 'RESET_CAM'); // .name('RESET ALL');
 
-f = gui.addFolder('Mass and Radius for Dynamics (Right Clic)');
-f.open();
+	// GitHub ICON 
+	var github = gui.add({ fun : function () { window.open('https://github.com/Almaric78/gravity-engine'); } }, 'fun').name('Github');
+	github.__li.className = 'cr function bigFont';
+	github.__li.style.borderLeft = '3px solid #8C8C8C';
+	var githubIcon = document.createElement('span');
+	github.domElement.parentElement.appendChild(githubIcon);
+	githubIcon.className = 'icon github';
 
-f.add(options, 'SPECIFIC_MASS', .00001, 10000.0).name("MASS");
-f.add(options, 'RADIUS', .001, 10.0);
-f.add(options, 'START_SPEED', 1e-100, 20.0);
-f.add(options, 'REMOVE_DYNAMICS').name('Remove dynamic balls');
+	/*
+		var twitter = gui.add({ fun : function () { window.open('https://twitter.com/..'); } }, 'fun').name('Twitter');
+		twitter.__li.className = 'cr function bigFont';
+		twitter.__li.style.borderLeft = '3px solid #8C8C8C';
+		var twitterIcon = document.createElement('span');
+		twitter.domElement.parentElement.appendChild(twitterIcon);
+		twitterIcon.className = 'icon twitter';
+	*/
 
-f = gui.addFolder('Start');
-f.open();
+	//console.log(gui);
+	//gui.close();
+	
+	return gui;
 
+}
 
-var fSpeedE = f.add(options, 'MOON_SPEED', 1e-100, 20.0);
-fSpeedE.onFinishChange(function (value) {
-    //reset();
-});
-
-var moveSpeed = 5;
-var fSpeedMS = f.add(options, 'MoveSpeed', 1, 100);
-fSpeedMS.onFinishChange(function (value) {
-    //console.log(value);
-    moveSpeed = Math.floor(options.MoveSpeed);
-    console.log("MoveSpeed: " + moveSpeed);
-});
-
-//f.add(options, 'AddBigStar').name('Add Big Star');
-f.add(options, 'SAVECONFIG').name('SaveTheConf');
-f.add(options, 'RESET').name('RESET ALL');
-
-// GitHub ICON 
-var github = gui.add({ fun : function () { window.open('https://github.com/Almaric78/gravity-engine'); } }, 'fun').name('Github');
-github.__li.className = 'cr function bigFont';
-github.__li.style.borderLeft = '3px solid #8C8C8C';
-var githubIcon = document.createElement('span');
-github.domElement.parentElement.appendChild(githubIcon);
-githubIcon.className = 'icon github';
-
-/*
-    var twitter = gui.add({ fun : function () { window.open('https://twitter.com/PavelDoGreat'); } }, 'fun').name('Twitter');
-    twitter.__li.className = 'cr function bigFont';
-    twitter.__li.style.borderLeft = '3px solid #8C8C8C';
-    var twitterIcon = document.createElement('span');
-    twitter.domElement.parentElement.appendChild(twitterIcon);
-    twitterIcon.className = 'icon twitter';
-*/
-
-//console.log(gui);
-//gui.close();
-
-
-//var G = 100;
-
-//var MOVER_COUNT = 32;
-
-//var MOVER_MASS_MIN = .0000001;
-//var MOVER_MASS_MAX = 100;
 
 ///* GET parameters for configuration: */
 //var GET_G = parseFloat(GET("G"));
@@ -175,7 +151,6 @@ var SPHERE_SIDES = 12;
 //var TRAILS_LENGTH = 100;
 
 var zoom = 1.0;
-var translate = new THREE.Vector3();
 
 var movers = [];
 //var container =  false; // ME
--- js/options.config.dat2.js
+++ js/options.config.dat2.js
@@ -3,129 +3,131 @@
 
 function InitGUIFromOptions(options){
 
-var optionsSvgName = options.NAME_SVG ? options.NAME_SVG : "options_EM" ;
+	var optionsSvgName = options.NAME_SVG ? options.NAME_SVG : "options_EM" ;
 
-// Force Reset Config ? 
-var httpReset = GET('R');
-if(httpReset){
-	console.log("Force Reset Initial Config:" + httpReset);
-	console.log("Initial Config :", options);
-}
-// LOAD CONFIG 
-else if (localStorage && localStorage.getItem(optionsSvgName)){
-    optionsSVG = JSON.parse(localStorage.getItem(optionsSvgName));
-	if(optionsSVG.NAME==options.NAME){
-		console.log("LOAD CONFIG:" + optionsSvgName);
-		for (var key in optionsSVG) {
-			console.log(key, optionsSVG[key]);
-			options.key = optionsSVG.key;
-			options[key] = optionsSVG[key];
+	// Force Reset Config ? 
+	var httpReset = GET('R');
+	if(httpReset){
+		console.log("Force Reset Initial Config:" + httpReset);
+		console.log("Initial Config :", options);
+	}
+	// LOAD CONFIG 
+	else if (localStorage && localStorage.getItem(optionsSvgName)){
+		optionsSVG = JSON.parse(localStorage.getItem(optionsSvgName));
+		if(optionsSVG.NAME==options.NAME){
+			console.log("LOAD CONFIG:" + optionsSvgName);
+			for (var key in optionsSVG) {
+				console.log(key, optionsSVG[key]);
+				options.key = optionsSVG.key;
+				options[key] = optionsSVG[key];
+			}
+		} else {
+			console.log("BAD LOAD CONFIG:" + optionsSVG.NAME + " - EXPECTED:" + options.NAME + " > NO LOAD");
+			console.log(options);
 		}
 	} else {
-		console.log("BAD LOAD CONFIG:" + optionsSVG.NAME + " - EXPECTED:" + options.NAME + " > NO LOAD");
-		console.log(options);
+		console.log("No Saved Config:" + optionsSvgName);
+		console.log("Initial Config :", options);
 	}
-} else {
-	console.log("No Saved Config:" + optionsSvgName);
-	console.log("Initial Config :", options);
-}
 
-options.SAVECONFIG = function () {
-    // SVG CONFIG OPTIONS
-    localStorage.setItem(optionsSvgName, JSON.stringify(options));
-	console.log("Saved Config:" + optionsSvgName);
-}
+	options.SAVECONFIG = function () {
+		// SVG CONFIG OPTIONS
+		localStorage.setItem(optionsSvgName, JSON.stringify(options));
+		console.log("Saved Config:" + optionsSvgName);
+	}
 
-// ---
+	// ---
 
-options.RESET = function () {
-    reset();
-}
+	options.RESET = function () {
+		reset();
+	}
 
-options.RESET_MOON_S = function () {
-    reinitMoonS();
-}
+	options.RESET_MOON_S = function () {
+		reinitMoonS();
+	}
 
-options.REMOVE_DYNAMICS = function () {
-    removeDynamicMovers();
-}
+	options.REMOVE_DYNAMICS = function () {
+		removeDynamicMovers();
+	}
 
-// dat GUI
-var gui = new dat.GUI();
-gui.remember(options);
+	// dat GUI
+	var gui = new dat.GUI();
+	gui.remember(options);
 
-var f = gui.addFolder('Environment');
-f.open();
-//f.add(options, 'framerate', 1, 120);
-f.add(options, 'G', 1, 100).name('Gravity');
+	var f = gui.addFolder('Environment');
+	f.open();
+	//f.add(options, 'framerate', 1, 120);
+	f.add(options, 'G', 1, 100).name('Gravity');
 
-/*
-	var fMoverCountE = f.add(options, 'MOVER_COUNT', 1, 128);
-	fMoverCountE.onFinishChange(function (value) {
-		// Fires when a controller loses focus.
+	/*
+		var fMoverCountE = f.add(options, 'MOVER_COUNT', 1, 128);
+		fMoverCountE.onFinishChange(function (value) {
+			// Fires when a controller loses focus.
+			//reset();
+		});
+	*/
+
+	f = gui.addFolder('Trails & Labels');
+	f.open();
+	f.add(options, 'TRAILS_DISPLAY');
+	f.add(options, 'TRAILS_LENGTH', 0, 10000);
+
+	f.add(options, 'SHOW_DIED');
+	f.add(options, 'SHOW_LABELS');
+
+	f = gui.addFolder('Mass and Radius for Dynamics (Right Clic)');
+	f.open();
+
+	f.add(options, 'SPECIFIC_MASS', .00001, 10000.0).name("MASS");
+	f.add(options, 'RADIUS', .001, 10.0);
+	f.add(options, 'START_SPEED', 1e-100, 20.0);
+	f.add(options, 'REMOVE_DYNAMICS').name('Remove dynamic balls');
+
+
+	f = gui.addFolder('Start');
+	f.open();
+
+	var fSpeedE = f.add(options, 'MOON_SPEED', 1e-100, 20.0);
+	fSpeedE.onFinishChange(function (value) {
 		//reset();
 	});
-*/
 
-f = gui.addFolder('Trails & Labels');
-f.open();
-f.add(options, 'TRAILS_DISPLAY');
-f.add(options, 'TRAILS_LENGTH', 0, 10000);
+	//var moveSpeed = 5;
+	f.add(options, 'MoveSpeed', 1, 100)
+	/*.onFinishChange(function (value) {
+		//console.log(value);
+		moveSpeed = Math.floor(options.MoveSpeed);
+		console.log("MoveSpeed: " + moveSpeed);
+	});
+	*/ 
 
-f.add(options, 'SHOW_DIED');
-f.add(options, 'SHOW_LABELS');
-
-f = gui.addFolder('Mass and Radius for Dynamics (Right Clic)');
-f.open();
-
-f.add(options, 'SPECIFIC_MASS', .00001, 10000.0).name("MASS");
-f.add(options, 'RADIUS', .001, 10.0);
-f.add(options, 'START_SPEED', 1e-100, 20.0);
-f.add(options, 'REMOVE_DYNAMICS').name('Remove dynamic balls');
+	//f.add(options, 'AddBigStar').name('Add Big Star');
+	f.add(options, 'SAVECONFIG').name('SaveTheConf');
+	f.add(options, 'RESET').name('RESET ALL');
+	f.add(options, 'RESET_MOON_S').name('New Simul Moon');
 
 
-f = gui.addFolder('Start');
-f.open();
+	// GitHub ICON 
+	var github = gui.add({ fun : function () { window.open('https://github.com/Almaric78/gravity-engine'); } }, 'fun').name('Github');
+	github.__li.className = 'cr function bigFont';
+	github.__li.style.borderLeft = '3px solid #8C8C8C';
+	var githubIcon = document.createElement('span');
+	github.domElement.parentElement.appendChild(githubIcon);
+	githubIcon.className = 'icon github';
+		
+	/*
+		var twitter = gui.add({ fun : function () { window.open('https://twitter.com/..'); } }, 'fun').name('Twitter');
+		twitter.__li.className = 'cr function bigFont';
+		twitter.__li.style.borderLeft = '3px solid #8C8C8C';
+		var twitterIcon = document.createElement('span');
+		twitter.domElement.parentElement.appendChild(twitterIcon);
+		twitterIcon.className = 'icon twitter';
+	*/
 
-var fSpeedE = f.add(options, 'MOON_SPEED', 1e-100, 20.0);
-fSpeedE.onFinishChange(function (value) {
-    //reset();
-});
+	//console.log(gui);
+	//gui.close();
 
-//var moveSpeed = 5;
-f.add(options, 'MoveSpeed', 1, 100)
-/*.onFinishChange(function (value) {
-    //console.log(value);
-    moveSpeed = Math.floor(options.MoveSpeed);
-    console.log("MoveSpeed: " + moveSpeed);
-});
-*/ 
-
-//f.add(options, 'AddBigStar').name('Add Big Star');
-f.add(options, 'SAVECONFIG').name('SaveTheConf');
-f.add(options, 'RESET').name('RESET ALL');
-f.add(options, 'RESET_MOON_S').name('New Simul Moon');
-
-
-// GitHub ICON 
-var github = gui.add({ fun : function () { window.open('https://github.com/Almaric78/gravity-engine'); } }, 'fun').name('Github');
-github.__li.className = 'cr function bigFont';
-github.__li.style.borderLeft = '3px solid #8C8C8C';
-var githubIcon = document.createElement('span');
-github.domElement.parentElement.appendChild(githubIcon);
-githubIcon.className = 'icon github';
-	
-/*
-    var twitter = gui.add({ fun : function () { window.open('https://twitter.com/PavelDoGreat'); } }, 'fun').name('Twitter');
-    twitter.__li.className = 'cr function bigFont';
-    twitter.__li.style.borderLeft = '3px solid #8C8C8C';
-    var twitterIcon = document.createElement('span');
-    twitter.domElement.parentElement.appendChild(twitterIcon);
-    twitterIcon.className = 'icon twitter';
-*/
-
-//console.log(gui);
-//gui.close();
+	return gui;
 
 }
 
